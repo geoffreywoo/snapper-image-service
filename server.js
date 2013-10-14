6367c0dc90cf3902bd048bcb0208b1f3de24e002
Geoffrey Woo <geoffreywoo@Geoffrey-Woos-MacBook-Air-3.local>
2013-10-13 17:47:47 -0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
+var azure = require('azure');
+
+var retryOperations = new azure.ExponentialRetryPolicyFilter();
+var blobService = azure.createBlobService().withFilter(retryOperations);
+
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('Hello Azure\n');
 }).listen(port);
