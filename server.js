945fe2a8693754e409f4e26f5cd3b0306e348ff9
Geoffrey Woo <geoffreywoo@Geoffrey-Woos-MacBook-Air-3.local>
2013-10-13 17:12:50 -0700

    test

--- server.js
+++ server.js
@@ -14,19 +14,9 @@ blobService.createContainerIfNotExists(containerName
         }
     });
 
-/*
-blobService.createBlockBlobFromFile(containerName
-    , 'test1'
-    , 'test1.txt'
-    , function(error){
-        if(!error){
-            // File has been uploaded
-        }
-    });
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
-*/
